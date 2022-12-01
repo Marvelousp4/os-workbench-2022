c0173de9ed4bf92f648053a71bda8b0b55c9aa13
tracer-nju <tracer@nju.edu.cn>
2022-12-01 17:32:03 +0800

    Linux DESKTOP-87U753D 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:32:03 up  3:07,  0 users,  load average: 0.19, 0.09, 0.02

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -15,7 +15,7 @@ bool showPid()
     token = strtok(buffer, " ");
     while (token != NULL) {
         printf("Token %s\n", token);
-        printf("Token+1 %s\n", token + 7);
+        printf("Token+1 %s\n", token + 8);
         token = strtok(NULL, " ");
     }
     pclose(fp);
