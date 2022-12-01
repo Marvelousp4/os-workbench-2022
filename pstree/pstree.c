bc2c149fccea57b875743af8639d2e1e1e463721
tracer-nju <tracer@nju.edu.cn>
2022-12-01 16:11:13 +0800

    Linux DESKTOP-87U753D 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:11:13 up  1:46,  0 users,  load average: 0.00, 0.00, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,26 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+int main(int argc, char* argv[])
+{
+    int result;
+    while ((result = getopt(argc, argv, "p::n::V::")) != -1) {
+        switch (result) {
+        case 'p':
+            printf("option=a, optopt=%c, optarg=%s\n", optopt, optarg);
+            break;
+        case 'n':
+            printf("option=b, optopt=%c, optarg=%s\n", optopt, optarg);
+            break;
+        case 'V':
+            printf("option=c, optopt=%c, optarg=%s\n", optopt, optarg);
+            break;
+        default:
+            printf("default, result=%c\n", result);
+            break;
+        }
+        printf("argv[%d]=%s\n", optind, argv[optind]);
+    }
+    return 0;
 }
