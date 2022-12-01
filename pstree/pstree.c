ae44cf637d700a45afeecfd0aec18dfa5dab37bd
tracer-nju <tracer@nju.edu.cn>
2022-12-01 17:06:40 +0800

    Linux DESKTOP-87U753D 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:06:40 up  2:42,  0 users,  load average: 0.07, 0.05, 0.01

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -38,7 +38,7 @@ bool showPid()
     // fgets(buffer, sizeof(buffer), fp);
     // printf("%s", buffer);
     // pclose(fp);
-    execlp("ls", "ls", NULL, NULL);
+    execlp("ls", "ls /proc/", NULL, NULL);
     return 0;
 }
 
