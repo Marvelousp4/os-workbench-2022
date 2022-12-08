3253addcc7eca028bbd5ed9b012e703b3378c068
tracer-nju <tracer@nju.edu.cn>
2022-12-08 15:33:26 +0800

    Linux DESKTOP-87U753D 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:33:26 up 22:03,  0 users,  load average: 0.03, 0.03, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,27 +5,71 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 // strace可以查看所有系统调用
 // cat /proc/12434/status | grep PPid查看父进程
-bool showPid()
+
+struct Process {
+    int pid; // 进程的编号
+    int ppid; // 进程的父进程编号
+    char name; // 进程的名字
+    Process** children; // 进程的子进程
+    int child_count; // 子进程的数量
+};
+typedef Process ProcessList[MAX_PROCESS];
+
+void print_process_tree(Process* process, int depth)
 {
+    // 输出进程信息
+    for (int i = 0; i < depth; i++)
+        printf("    ");
+    printf("%d %s\n", process->pid, process->name);
+
+    // 递归输出子进程
+    for (int i = 0; i < process->child_count; i++)
+        print_process_tree(process->children[i], depth + 1);
+}
+
+void free_process_tree(Process* process)
+{
+    // 递归释放子进程
+    for (int i = 0; i < process->child_count; i++)
+        free_process_tree(process->children[i]);
+    free(process->children);
+    free(process);
+}
+
+int get_process_list(ProcessList* process_list, int max_count)
+{
+    int count = 0;
+
+    // 打开 /proc 目录
     DIR* dir = opendir("/proc");
     if (!dir) {
         perror("opendir");
         return 0;
     }
+
+    // 遍历目录下的所有文件
     struct dirent* ent;
     while ((ent = readdir(dir)) != NULL) {
+        // 跳过非目录文件
         if (ent->d_type != DT_DIR)
             continue;
+
+        // 获取进程号
         int pid = atoi(ent->d_name);
         if (pid == 0)
             continue;
+
+        // 获取进程的状态文件
         char path[64];
         sprintf(path, "/proc/%d/status", pid);
         FILE* fp = fopen(path, "r");
         if (!fp)
             continue;
+
+        // 读取进程的名称
         char name[64];
         int ret = fscanf(fp, "Name: %s", name);
         if (ret < 0) {
@@ -34,31 +78,110 @@ bool showPid()
             continue;
         }
         fclose(fp);
-        printf("PID: %d, Name: %s", pid, name);
+
+        // 读取进程的父进程号
+        fp = fopen(path, "r");
+        if (!fp)
+            continue;
+        int ppid = -1;
+        ret = fscanf(fp, "PPid: %d", &ppid);
+        if (ret < 0) {
+            perror("fscanf");
+            fclose(fp);
+            continue;
+        }
+        fclose(fp);
+
+        // 保存进程信息
+        process_list[count].pid = pid;
+        process_list[count].ppid = ppid;
+        strcpy(process_list[count].name, name);
+        count++;
     }
     closedir(dir);
-    return 1;
-    // FILE* fp;
-    // char buf[80];
-    // char* token;
-    // int process[1000];
-    // memset(process, 0, sizeof(int) * 1000);
-    // int pid, count = 0;
-    // fp = popen("ls /proc/", "r");
-    // while (fgets(buf, sizeof(buf), fp) != 0) {
-    //     pid = atoi(buf);
-    //     if (pid) {
-    //         process[count++] = pid;
-    //         printf("%d\n", process[count - 1]);
-    //     }
-    //     memset(buf, 0x0, sizeof buf);
-    // }
-    // pclose(fp);
-    // return 0;
+    return count;
 }
 
+bool showPid()
+{
+    // 获取进程列表
+    ProcessList process_list[1024];
+    int count = get_process_list(process_list, 1024);
+    if (count == 0)
+        return false;
+
+    // 构建进程树
+    Process* root = NULL;
+    for (int i = 0; i < count; i++) {
+        Process* process = (Process*)malloc(sizeof(Process));
+        process->pid = process_list[i].pid;
+        process->ppid = process_list[i].ppid;
+        strcpy(process->name, process_list[i].name);
+        process->children = NULL;
+        process->child_count = 0;
+
+        // 找到根进程
+        if (process->ppid == 0)
+            root = process;
+
+        // 找到父进程
+        for (int j = 0; j < count; j++) {
+            if (process_list[j].pid == process->ppid) {
+                Process* parent = (Process*)malloc(sizeof(Process));
+                parent->pid = process_list[j].pid;
+                parent->ppid = process_list[j].ppid;
+                strcpy(parent->name, process_list[j].name);
+                parent->children = NULL;
+                parent->child_count = 0;
+
+                // 添加子进程
+                parent->child_count++;
+                parent->children = (Process**)realloc(parent->children, sizeof(Process*) * parent->child_count);
+                parent->children[parent->child_count - 1] = process;
+                break;
+            }
+        }
+    }
+
+    // 输出进程树
+    print_process_tree(root, 0);
+
+    // 释放进程树
+    free_process_tree(root);
+    return true;
+}
+
+/*
+在原来的代码中，虽然只执行了一次 ls /proc/ 命令，但是在执行过程中会读取多次 /proc 目录。
+例如，每次执行 fgets 函数都会从 /proc 目录中读取一行数据，并将其保存到缓冲区中。
+这样一来，尽管只执行了一次 ls 命令，但是实际上会多次读取 /proc 目录，因此也会占用较多的系统资源。
+
+与此相比，上面的代码通过直接打开 /proc 目录并读取文件夹来获取进程编号，只需要读取一次 /proc 目录。
+这样一来，可以节省大量的读取次数，从而减少对系统资源的占用。
+*/
+// FILE* fp;
+// char buf[80];
+// char* token;
+// int process[1000];
+// memset(process, 0, sizeof(int) * 1000);
+// int pid, count = 0;
+// fp = popen("ls /proc/", "r");
+// while (fgets(buf, sizeof(buf), fp) != 0) {
+//     pid = atoi(buf);
+//     if (pid) {
+//         process[count++] = pid;
+//         printf("%d\n", process[count - 1]);
+//     }
+//     memset(buf, 0x0, sizeof buf);
+// }
+// pclose(fp);
+// return 0;
+
+// -n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。
 bool numericSort()
 {
+    // 按照pid的数值从小到大顺序输出一个进程的直接孩子。
+
     return 0;
 }
 
